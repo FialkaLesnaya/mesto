8957da0cce7359da5b59569c880286ab139c683b
Fialka Lesnaya <listik.vs@gmail.com>
2022-11-19 01:30:32 +0200

    Исправление ошибок

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,5 @@
 // Находим форму в DOM
-let formElement = document.querySelector('.popup__container'); // Воспользуйтесь методом querySelector()
+let formElement = document.querySelector('form[name="edit-profile"]'); // Воспользуйтесь методом querySelector()
 // Находим поля формы в DOM
 let nameInput = document.querySelector('.popup__input[name="name"]'); // Воспользуйтесь инструментом .querySelector()
 let jobInput = document.querySelector('.popup__input[name="job"]');// Воспользуйтесь инструментом .querySelector()
@@ -23,7 +23,7 @@ function formSubmitHandler(evt) {
     // Вставьте новые значения с помощью textContent
     nameText.textContent = nameInput.value;
     jobText.textContent = jobInput.value;
-    popupElement.classList.remove('popup__opened');
+    popupElement.classList.remove('popup_opened');
 }
 
 function editButtonHandler(evt) {
@@ -31,12 +31,12 @@ function editButtonHandler(evt) {
 
     nameInput.value = nameText.textContent;
     jobInput.value = jobText.textContent;
-    popupElement.classList.add('popup__opened');
+    popupElement.classList.add('popup_opened');
 }
 
 function closeButtonHandler(evt) {
     evt.preventDefault();
-    popupElement.classList.remove('popup__opened');
+    popupElement.classList.remove('popup_opened');
 }
 
 // Прикрепляем обработчик к форме:
