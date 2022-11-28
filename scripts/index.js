8a79cc69ab11edcbee27a6fc5324d905b8cc7937
Fialka Lesnaya <listik.vs@gmail.com>
2022-11-28 20:07:48 +0200

    Добавление шести карточек

--- scripts/index.js
+++ scripts/index.js
@@ -44,3 +44,49 @@ function closeButtonHandler(evt) {
 formElement.addEventListener('submit', formSubmitHandler);
 editButtonElement.addEventListener('click', editButtonHandler);
 closeButtonElement.addEventListener('click', closeButtonHandler);
+
+
+const cardsContainer = document.querySelector('.elements');
+
+function addCard(nameValue, linkValue) {
+    const cardTemplate = document.querySelector('#card-template').content;
+    const cardElement = cardTemplate.querySelector('.elements__item').cloneNode(true);
+    const cardName = cardElement.querySelector('.elements__name');
+    const cardImage = cardElement.querySelector('.elements__image');
+
+    cardName.textContent = nameValue;
+    cardName.setAttribute('title', nameValue);
+    cardImage.setAttribute('src', linkValue);
+    cardImage.setAttribute('alt', nameValue);
+
+    cardsContainer.append(cardElement);
+}
+
+const initialCards = [
+    {
+        name: 'Хорген,Швейцария',
+        link: './images/ricardo-gomez-angel-eis_TT6ntfo-unsplash.jpg'
+    },
+    {
+        name: 'Нью-Гэмпшир,США',
+        link: './images/balazs-busznyak-El5zuQAtfeo-unsplash.jpg'
+    },
+    {
+        name: 'Лаго-ди-Фузине,Италия',
+        link: './images/saso-tusar-s-k-2N90yuY-unsplash.jpg'
+    },
+    {
+        name: 'Гатлинбург,США',
+        link: './images/chad-madden-cPa-7yByq3o-unsplash.jpg'
+    },
+    {
+        name: 'Ферма Сидр-Хилл,США',
+        link: './images/bonnie-kittle-XAsG0EZEsyA-unsplash.jpg'
+    },
+    {
+        name: 'Лак-дю-Фламбо,США',
+        link: './images/dave-hoefler-Z9d7CYpBDqo-unsplash.jpg'
+    },
+];
+
+initialCards.forEach(card => addCard(card.name, card.link));
