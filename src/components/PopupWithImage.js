3db9974b3e26d2a9a84cfef6b95ee7021f9189bd
Fialka Lesnaya <listik.vs@gmail.com>
2023-02-11 19:45:39 +0200

    Исправление ошибок

--- src/components/PopupWithImage.js
+++ src/components/PopupWithImage.js
@@ -1,6 +1,6 @@
- import Popup from "./Popup.js";
- 
- export default class PopupWithImage extends Popup {
+import Popup from "./Popup.js";
+
+export default class PopupWithImage extends Popup {
     constructor(popupSelector) {
         super(popupSelector);
         this._subtitleElement = this._popup.querySelector('.popup__subtitle');
@@ -12,7 +12,6 @@
         this._popupImage.setAttribute('src', imageData.link);
         this._popupImage.setAttribute('alt', imageData.name);
         this._subtitleElement.textContent = imageData.name;
-        this._popup.classList.toggle('popup_image-overlay');
 
     }
 }
