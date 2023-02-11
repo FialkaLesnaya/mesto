e9c0ec9d00afc0e8658ec2c065d13c8ec4a0da23
Fialka Lesnaya <listik.vs@gmail.com>
2023-02-11 19:31:02 +0200

    Исправление ошибок

--- src/components/Api.js
+++ src/components/Api.js
@@ -4,6 +4,10 @@ export default class Api {
         this.headers = options.headers;
     }
 
+    _request(url, options) {
+        return fetch(url, options).then(this._checkResponse)
+    }
+
     _checkResponse(res) {
         if (res.ok) {
             return res.json();
@@ -13,87 +17,65 @@ export default class Api {
         return Promise.reject(`Ошибка: ${res.status}`);
     }
 
-    _logError(err) {
-        console.log(`Случилась ошибка ${err}`); // выведем ошибку в консоль
-    }
-
     getCurrentUser() {
-        return fetch(`${this.baseUrl}/users/me`, { headers: this.headers })
-            .then(res => this._checkResponse(res))
-            .catch(err => this._logError(err));
+        return this.request(`${this.baseUrl}/users/me`, { headers: this.headers });
     }
 
     loadCards() {
-        return fetch(`${this.baseUrl}/cards`, { headers: this.headers })
-            .then(res => this._checkResponse(res))
-            .catch(err => this._logError(err));
+        return this.request(`${this.baseUrl}/cards`, { headers: this.headers });
     }
 
     editProfile(name, about) {
-        return fetch(`${this.baseUrl}/users/me`, {
+        return this.request(`${this.baseUrl}/users/me`, {
             method: 'PATCH',
             headers: this.headers,
             body: JSON.stringify({
                 name: name,
                 about: about
             })
-        })
-            .then(res => this._checkResponse(res))
-            .catch(err => this._logError(err));
+        });
     }
 
     editCard(name, link) {
-        return fetch(`${this.baseUrl}/cards`, {
+        return this.request(`${this.baseUrl}/cards`, {
             method: 'POST',
             headers: this.headers,
             body: JSON.stringify({
                 name: name,
                 link: link,
             })
-        })
-            .then(res => this._checkResponse(res))
-            .catch(err => this._logError(err));
+        });
     }
 
     deleteCard(cardId) {
-        return fetch(`${this.baseUrl}/cards/${cardId}`, {
+        return this.request(`${this.baseUrl}/cards/${cardId}`, {
             method: 'DELETE',
             headers: this.headers,
 
-        })
-            .then(res => this._checkResponse(res))
-            .catch(err => this._logError(err));
+        });
     }
 
     likeCard(cardId) {
-        return fetch(`${this.baseUrl}/cards/${cardId}/likes`, {
+        return this.request(`${this.baseUrl}/cards/${cardId}/likes`, {
             method: 'PUT',
             headers: this.headers,
-
-        })
-            .then(res => this._checkResponse(res))
-            .catch(err => this._logError(err));
+        });
     }
 
     deleteLikeCard(cardId) {
-        return fetch(`${this.baseUrl}/cards/${cardId}/likes`, {
+        return this.request(`${this.baseUrl}/cards/${cardId}/likes`, {
             method: 'DELETE',
             headers: this.headers,
-
-        })
-            .then(res => this._checkResponse(res))
-            .catch(err => this._logError(err));
+        });
     }
 
     updateAvatar(avatar) {
-        return fetch(`${this.baseUrl}/users/me/avatar`, {
+        return this.request(`${this.baseUrl}/users/me/avatar`, {
             method: 'PATCH',
             headers: this.headers,
             body: JSON.stringify({
                 avatar: avatar,
             })
-        })
-            .then(res => this._checkResponse(res))
-            .catch(err => this._logError(err));
+        });
     }
 }
